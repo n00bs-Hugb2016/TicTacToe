de36d6c2e71564819ea2552e73bcba5a36dae511
Julia O <juliao15@i-1217c9a8.cs1004cloud.internal>
2016-10-24 20:23:18 +0000

    Tried to get gradle to work. Still WIP.

---
+++ src/main/java/is/n00bs/temp/tempHelloWorld.java
@@ -0,0 +1,8 @@
+package is.n00bs.temp;
+
+public class tempHelloWorld {
+  public static void Main(String[] args) {
+    System.out.println("Hello World");
+  }
+
+}
---
+++ src/test/java/is/n00bs/temp/tempTest.java
@@ -0,0 +1,8 @@
+package is.n00bs.temp;
+
+public class tempTest {
+  public static void Main(String[] args){
+
+  }
+
+}
