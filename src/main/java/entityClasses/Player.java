007d9fd242d4e06b9d4a486d0eb89c68fe1dc172
tinnathuridur <tinnath@Tinnas-MacBook-Pro.local>
2016-10-29 17:50:18 +0000

    T&S documented the Player class

--- src/main/java/entityClasses/Player.java
+++ src/main/java/entityClasses/Player.java
@@ -1,5 +1,9 @@
 package entityClasses;
 
+/**
+ * This is an entity class, representing a player.
+ */
+
 public class Player
 {
     private int movesMade;
@@ -13,16 +17,32 @@ public class Player
         wins = 0;
     }
     
+    /**
+     * Returns the number of moves made by this player.
+     * @return    the number of moves made
+     * @see       int
+     */
     public int getMoves ()
     {
         return movesMade;
     }
 
+    /**
+     * Returns this players sign.
+     * @return    this player's sign
+     * @see       char
+     */
     public char getSign()
     {
         return sign;
     }
 
+    /**
+     * Makes a move on the board if it is leagal.
+     * @param    board    the game board
+     * @param    x        the x position
+     * @param    y        the y position
+     */
     public void makeMove(Board board, int x, int y) 
     {
         if (board.getPositionChar(x, y) == ' ') 
