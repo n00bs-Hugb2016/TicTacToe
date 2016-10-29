a0b4f3fa9c0d842c3b268a3dc80d6e82d7792981
Berglind Ósk Einarsdóttir <berglind15@ru.is>
2016-10-29 19:29:37 +0000

    DB: GameController connecting other classes

---
+++ src/main/java/businessLayer/GameController.java
@@ -0,0 +1,21 @@
+package businessLayer;
+
+
+/**
+ * This is a business class, representing the game.
+ */
+
+public class GameController {
+
+	private Player _playerX;
+	private Player _playerO;
+	private Board gameBoard;
+
+	
+	public GameController()
+	{
+		/*_playerX = new Player('X');
+		_playerO = new Player('O');
+		gameBoard = new Board();*/
+	}
+}
---
+++ src/test/java/businessLayer/GameControllerTest.java
@@ -0,0 +1,10 @@
+package businessLayer;
+
+public class GameControllerTest {
+
+	public static void main(String args[])
+	{
+		org.junit.runner.JUnitCore.main("businessLayer");
+	}
+
+}
