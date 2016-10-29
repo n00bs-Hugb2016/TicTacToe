5fbbd61c0d544be884b928f444a0472b15aa4165
Drifa <drifa15@ru.is>
2016-10-29 14:00:34 +0000

    DB addedd functions checkFullBoard(), checkBoardWinner() and tested checkBoardWinner()

--- src/main/java/entityClasses/Board.java
+++ src/main/java/entityClasses/Board.java
@@ -4,14 +4,14 @@ public class Board
 {
 
     public static char[][] boardArray;
-    public static int arrSize;
+    public static int boardSize;
 	public static char playerX = 'X';
 	public static char playerO = 'O';
 
     public Board()
     {
-        arrSize = 3;
-        boardArray = new char[arrSize][arrSize];
+        boardSize = 3;
+        boardArray = new char[boardSize][boardSize];
 		fillBoard();
     }
 	
@@ -22,14 +22,15 @@ public class Board
 	
 	public static void fillBoard()
 	{
-		for(int i = 0; i < arrSize; i++)
+		for(int i = 0; i < boardSize; i++)
 		{
-			for(int j = 0; j < arrSize; j++)
+			for(int j = 0; j < boardSize; j++)
 			{
 				boardArray[i][j] = ' ';
 			}
 		}
 	}
+	
 	public static char getPosition(int x, int y)
 	{
 		return boardArray[x][y];
@@ -39,4 +40,53 @@ public class Board
 	{
 		boardArray[x][y] = playerChar;
 	}
+	
+	public static boolean checkBoardWinner(char playerChar)
+	{
+		//check rows
+		if(boardArray[0][0] == boardArray[0][1]&& boardArray[0][1] == boardArray[0][2] && boardArray[0][0] == playerChar)
+			return true;
+		if(boardArray[1][0] == boardArray[1][1]&& boardArray[1][1] == boardArray[1][2] && boardArray[1][0] == playerChar)
+			return true;
+		if(boardArray[2][0] == boardArray[2][1]&& boardArray[2][1] == boardArray[2][2] && boardArray[2][0] == playerChar)
+			return true;
+		
+		//check columns
+		if(boardArray[0][0] == boardArray[1][0]&& boardArray[1][0] == boardArray[2][0] && boardArray[0][0] == playerChar)
+			return true;
+		if(boardArray[0][1] == boardArray[1][1]&& boardArray[1][1] == boardArray[2][1] && boardArray[0][1] == playerChar)
+			return true;
+		if(boardArray[0][2] == boardArray[1][2]&& boardArray[1][2] == boardArray[2][2] && boardArray[0][2] == playerChar)
+			return true;
+		
+		//check corners
+		if(boardArray[0][0] == boardArray[1][1]&& boardArray[1][1] == boardArray[2][2] && boardArray[0][0] == playerChar)
+			return true;
+		if(boardArray[0][2] == boardArray[1][1]&& boardArray[1][1] == boardArray[2][0] && boardArray[0][2] == playerChar)
+			return true;
+		
+		return false;
+	}
+	
+	
+	public static boolean checkFullBoard()
+	{
+		for(int i = 0; i < boardSize; i++)
+		{
+			for(int j = 0; j < boardSize; j++)
+			{
+				if(boardArray[i][j] == ' ')
+				{
+					//the board is not full
+					return false;
+				}
+			}
+		}
+		//the board is full
+		return true;
+	}
+	
+	
+	
 }
+
--- src/test/java/entityClasses/BoardTest.java
+++ src/test/java/entityClasses/BoardTest.java
@@ -16,9 +16,9 @@ public class BoardTest
     {
 		Board b  = new Board();
 		
-		for(int i = 0; i < b.arrSize; i++)
+		for(int i = 0; i < b.boardSize; i++)
 		{
-			for(int j = 0; j < b.arrSize; j++)
+			for(int j = 0; j < b.boardSize; j++)
 			{
 				assertEquals(' ', b.boardArray[i][j]);
 			}
@@ -31,5 +31,27 @@ public class BoardTest
 		b.setPosition(1, 1, b.playerX);
 		assertEquals('X', b.getPosition(1, 1));
 	}
+	@Test
+	public void testWinner()
+	{
+		Board b = new Board();
+		b.setPosition(0,0, b.playerX);
+		b.setPosition(0,1, b.playerX);
+		b.setPosition(0,2, b.playerX);
+		assertEquals(true, b.checkBoardWinner(b.playerX));
+		
+		b.fillBoard();
+		b.setPosition(0,1, b.playerX);
+		b.setPosition(1,1, b.playerX);
+		b.setPosition(2,1, b.playerX);
+		assertEquals(true, b.checkBoardWinner(b.playerX));
+		
+		b.fillBoard();
+		b.setPosition(0,0, b.playerO);
+		b.setPosition(1,1, b.playerO);
+		b.setPosition(2,2, b.playerO);
+		assertEquals(true, b.checkBoardWinner(b.playerO));
+	}
+
 }
 
