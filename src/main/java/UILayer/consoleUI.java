2aeb2ab43f5fe7d7fb22e5740c8eb5097a45fd75
Berglind Ósk Einarsdóttir <berglind15@ru.is>
2016-10-29 20:50:28 +0000

    DB: Made UI console class

---
+++ src/main/java/UILayer/consoleUI.java
@@ -0,0 +1,18 @@
+package UILayer;
+
+import businessLayer.*;
+
+public class consoleUI {
+
+	public static void main(String args[])
+	{
+		/*System.out.println("What is name of player X: ");
+		String name = System.readLine();
+		System.out.println("What is name of player O: ");
+		Player p1 = new Player();
+		Player p2 = new Player();
+
+		System.out.println("Welcome to TicTacToe!");
+		*/
+	}
+}
