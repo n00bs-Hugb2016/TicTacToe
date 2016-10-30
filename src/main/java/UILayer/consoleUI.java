decdd7e4002402467acfb59ef1fe5cd90762f374
tinnathuridur <tinnath@Tinnas-MacBook-Pro.local>
2016-10-30 19:33:35 +0000

    B&D&T working on the console UI, now printing the board

--- src/main/java/UILayer/consoleUI.java
+++ src/main/java/UILayer/consoleUI.java
@@ -4,41 +4,66 @@ import businessLayer.*;
 import entityClasses.*;
 import java.util.*;
 import java.io.*;
-
+//import java.util.Scanner;
 
 
 public class consoleUI 
 {
+	private static GameController game;
+
 	public static void main(String args[])
 	{
 		Scanner scanIn = new Scanner(System.in);
-		//Scanner scanOut = new Scanner(System.out);
 
 		System.out.println("Welcome to the game TicTacToe!");
-		
-		System.out.print("What is name of player X: ");
-		String nameX = scanIn.nextLine();
 
-		System.out.print("What is name of player O: ");
+		System.out.println("What is name of player X: ");
+		String nameX = scanIn.nextLine();
+		System.out.println("What is name of player O: ");
 		String nameO = scanIn.nextLine();
 		
-		GameController game = new GameController(nameX, nameO);
+		game = new GameController(nameX, nameO);
+
+		printBoard();
 
 		System.out.println("Let's start the game!");
 
 		Boolean stopGame = false;
-		int move;
-		String bla;
+		int moveX;
+		int moveY;
+
 		while(stopGame == false)
 		{
-			System.out.println("Write first x position then y position");
-			//System.out.println("Make your move " + game.getWhoStarts().getName());
-			System.out.println("Make your move ");
-			move = scanIn.nextInt();
-			scanIn.nextLine();
+			System.out.println("Make your move " + game.getCurrentPlayer().getName());
+			System.out.println("Write first the x position: ");
+			moveX = scanIn.nextInt();
+		    System.out.println("Now write the y position: ");
+			moveY = scanIn.nextInt();
+			game.makeMove(moveX, moveY);
 			stopGame = true;
 		}
 		scanIn.close();
+		
+	}
+
+
+
+	private static void printBoard() 
+	{
+		char[][] board = game.getBoard();
+
+
+		System.out.println("     |     |     ");
+		System.out.println("  " + board[0][0] + "  | " + board[0][1] + "   | " + board[0][2]);
+		System.out.println("     |     |     ");
+		System.out.println("-----+-----+-----");
+		System.out.println("     |     |     ");
+		System.out.println("  " + board[1][0] + "  | " + board[1][1] + "   | " + board[1][2]);
+		System.out.println("     |     |     ");
+		System.out.println("-----+-----+-----");
+		System.out.println("     |     |     ");
+		System.out.println("  " + board[2][0] + "  | " + board[2][1] + "   | " + board[2][2]);
+		System.out.println("     |     |     ");
 		
 	}
 }
