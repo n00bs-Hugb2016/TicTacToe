26d45f59c374c17d3431d600df47e32ee0e59f96
Berglind Ósk Einarsdóttir <berglind15@ru.is>
2016-10-30 09:29:51 +0000

    DB working on display in UI, console

--- src/main/java/UILayer/consoleUI.java
+++ src/main/java/UILayer/consoleUI.java
@@ -1,18 +1,23 @@
 package UILayer;
 
 import businessLayer.*;
+import entityClasses.*;
+import java.util.*;
+import java.io.*;
+
 
 public class consoleUI {
 
 	public static void main(String args[])
 	{
-		/*System.out.println("What is name of player X: ");
-		String name = System.readLine();
+		System.out.println("What is name of player X: ");
+		//String nameX = System.readLine();
 		System.out.println("What is name of player O: ");
-		Player p1 = new Player();
-		Player p2 = new Player();
+		//String nameO = System.readLine();
+		Player playerX = new Player('X');
+		Player playerO = new Player('O');
 
 		System.out.println("Welcome to TicTacToe!");
-		*/
+		
 	}
 }
