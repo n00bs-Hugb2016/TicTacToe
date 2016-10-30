7cdcf696acec736acde1e2ff32ce73df0560c82c
Drifa <drifa15@ru.is>
2016-10-30 17:47:06 +0000

    getting on track with the consoleUI class

--- src/main/java/UILayer/consoleUI.java
+++ src/main/java/UILayer/consoleUI.java
@@ -4,33 +4,29 @@ import businessLayer.*;
 import entityClasses.*;
 import java.util.*;
 import java.io.*;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 
 public class consoleUI 
 {
 	public static void main(String args[])
 	{
-		GameController game = new GameController();
 		Scanner scanIn = new Scanner(System.in);
-
-		//Console console = System.console();
 
 		System.out.println("Welcome to the game TicTacToe!");
 		
 		System.out.println("What is name of player X: ");
-		//String nameX = scanIn.nextLine();
-		String nameX = "John";
-		//game.getPlayer('X').name = nameX;
+		String nameX = scanIn.nextLine();
 		System.out.println("What is name of player O: ");
-		//String nameO = scanIn.nextLine();
-		String nameO = "Sue";
-		//game.getPlayer('O').name = nameO;
+		String nameO = scanIn.nextLine();
 		
+		GameController game = new GameController(nameX, nameO);
+
 		System.out.println("Let's start the game!");
 
 		Boolean stopGame = false;
 		int move;
+		String bla;
 		while(stopGame == false)
 		{
 			System.out.println("Write first x position then y position");
@@ -39,8 +35,7 @@ public class consoleUI
 			move = scanIn.nextInt();
 			stopGame = true;
 		}
-
-
+		scanIn.close();
 		
 	}
 }
